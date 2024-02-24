6b8415dc6ad361c1caabba9ea02c5e32bda1820a
Максим Арсаев <member2005@yandex.ru>
2024-02-24 16:28:21 +0300

    Ещё раз сеттинг переделал.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram_password'),
         'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
